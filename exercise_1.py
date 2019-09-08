a22fbfbe6a51b947c258b32559b42b987ea81e2d
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-09-08 20:08:38 -0300

    Alternativa de un ejercicio

--- exercise_1.py
+++ exercise_1.py
@@ -44,12 +44,22 @@ print("D) Suma de pares: ",suma_pares)
 """ E) """
 print("\nEjercicio E:")
 suma=0
-lista =[1,2,500,96,40,85,99,105,26,28]
+lista =[1,2,500,96,40,15,5,50,20,10]
 
 for i in range(10):
     if (i>4):
         suma+= lista[i]
 print("La suma de las ultimas 5 cifras es: ",suma)
+
+""" E2) """
+print("\nEjercicio E version 2:")
+suma=0
+for i in range(10):
+    numero = int(input(str(i)+ ". Ingrese un numero para sumar los ultimos 5: "))
+    if (i>4):
+        suma+=numero
+print("La suma de los ultimos 5 numeros es: ", suma)
+    
 
 
 """ F) """
