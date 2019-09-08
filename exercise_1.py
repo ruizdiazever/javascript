abbe422dd3373096d5162fb1750624aa9d2bf9a9
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-09-08 11:50:05 -0300

    Python, ultimo problema, E: en progreso

--- exercise_1.py
+++ exercise_1.py
@@ -1,7 +1,6 @@
 """ 
+Se realiza la carga de 10 valores enteros por teclado. Se desea conocer:
 For
-    Se realiza la carga de 10 valores enteros por teclado. Se desea conocer:
-
     A)LISTO 	La cantidad de valores negativos ingresados.
     B)LISTO 	La cantidad de valores positivos ingresados.
     C)LISTO 	La cantidad de múltiplos de 15.
@@ -10,19 +9,14 @@ For
     F)LISTO     Desarrollar un programa que muestre la tabla de multiplicar de un número ingresado por teclado.
 
 While
-    G)  Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc. (No se ingresan valores por teclado).
-    H)  Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8 -16 -24, etc. 
+    G)LISTO     Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc. (No se ingresan valores por teclado).
+    H)LISTO     Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8 -16 -24, etc. 
 """
 
 
 
-
-
-""" A) 	La cantidad de valores negativos ingresados.
-    B) 	La cantidad de valores positivos ingresados.
-    C) 	La cantidad de múltiplos de 15.
-    D) 	El valor acumulado de los números ingresados que son pares. """
-
+print("Ejercicios A, B, C & D:")
+""" A) B) C) D) """
 numeroInput=0
 contador_negativo=0
 contador_positivo=0
@@ -41,7 +35,7 @@ for i in range(10):
     if numeroInput%2==0:
         suma_pares+=numeroInput
 
-print("A) Numeros negativos: ",contador_negativo)
+print("\nA) Numeros negativos: ",contador_negativo)
 print("B) Numeros positivos: ",contador_positivo)
 print("c) Multiplos de 15: ",contador_multiplo)
 print("D) Suma de pares: ",suma_pares)
@@ -49,12 +43,8 @@ print("D) Suma de pares: ",suma_pares)
 
 
 
-
-
-
-""" print("*************") """
-""" E) Desarrollar un programa que solicite la carga de 10 números e imprima la 
-    suma de lo últimos 5 valores ingresados """
+""" E) """
+print("\nEjercicio E:")
 """ numeroInput_2=[]
 suma_py=0
 suma_valores_2=0
@@ -65,9 +55,8 @@ for i in range(10):
 
 
 
-
-
-""" F) Desarrollar un programa que muestre la tabla de multiplicar de un número ingresado por teclado. """
+""" F) """
+print("\nEjercicio F, tabla de multiplicar:")
 tabla=0
 numeroInput_3= int(input("Ingrese un numero: "))
 for i in range (1,11,1):
@@ -75,14 +64,24 @@ for i in range (1,11,1):
     print("F) ",numeroInput_3,"x",i,"= ",tabla,)
 
 
+""" G) """
+print("\nEjercicio G, 25 terminos de 11:")
+contador_max = 0
+numero = 0
+aux = 11
+while (contador_max < 25):
+    numero= numero + aux
+    contador_max+=1
+    print(contador_max,"- Termino: ",numero)
 
 
-""" G) Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en 
-    pantalla 8 -16 -24, etc. """
+
+
+""" H """
+print("\nEjercicio H, multiplos de 8 hasta 500:")
 multiplo=8
 numero=8
 maximo=500
-print("Multiplos de 8 hasta el 500: ")
 while multiplo <= maximo:
     print(multiplo)
     multiplo = numero+multiplo
