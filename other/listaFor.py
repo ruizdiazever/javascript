0ca2b503c8ac491dfb834096d24bfd6abbd3bc48
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-09-09 18:00:09 -0300

    Reestructuracion

--- other/listaFor.py
+++
@@ -1,9 +0,0 @@
-nElementos = int(input("Ingrese porfavor la dimension de la lista: "))
-lista = list(range(0, nElementos))
-suma=0
-
-for i in range(nElementos):
-    lista = int(input(str(i+1)+". Ingrese porfavor 10 numeros enteros: "))
-    if (i>4):
-        suma+=lista
-print("La suma de los ultimos 5 numeros es: ",suma)
--- other/tests.py
+++ other/tests.py
@@ -35,3 +35,17 @@ print("**********************************************")
 contador=0
 for i in range(2,20,2):
     print("El numero ",i," es par.")
+
+
+
+
+print("**********************************************")
+nElementos = int(input("Ingrese porfavor la dimension de la lista: "))
+lista = list(range(0, nElementos))
+suma=0
+
+for i in range(nElementos):
+    lista = int(input(str(i+1)+". Ingrese porfavor 10 numeros enteros: "))
+    if (i>4):
+        suma+=lista
+print("La suma de los ultimos 5 numeros es: ",suma)
