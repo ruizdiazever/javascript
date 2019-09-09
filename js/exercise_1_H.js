88ec69466cb07bad0c01abdb61e42ba126f13093
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-09-09 18:39:29 -0300

    Cambios ligeros

--- js/exercise_1_H.js
+++ js/exercise_1_H.js
@@ -4,7 +4,7 @@ var multiplo = 0;
         var numero = 8;
         var maximo = 500;
         document.write("Multiplos de 8 hasta el 500: <br>");
-        while (multiplo <= maximo){
+        while (multiplo < maximo){
             multiplo = multiplo+numero;
             document.write(multiplo+"<br>")
         }
