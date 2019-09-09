8887acfb1d74b642168eb507d122d6bc9b7b72cd
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-09-09 20:47:54 -0300

    Cambios

--- ejercicio_2/js/exercise_2.js
+++ ejercicio_2/js/exercise_2.js
@@ -7,11 +7,17 @@ numero3=parseInt(prompt('Ingrese porfavor un numero: '));
 menorMayor(numero1, numero2, numero3);
 
 function menorMayor(num1, num2, num3){
-    if (num1 < num2 && num2 < num3 ){
+    if ( (num1<num2) && (num2 < num3) ){
+
         document.write(num,", ",num2,", ",num3)
-    }else if (num2 < num1 && num1 < num3){
+
+    }else if ( (num2<num1) && (num1<num3) ){
+
         document.write(num2,", ",num1,", ",num3)
+
     }else{
+
         document.write(num3,", ",num2," ,",num1)
+        
     }
 }
