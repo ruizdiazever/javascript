26e554a5081c1e05e603985ddacf3c418c6a7fac
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-09-08 21:50:04 -0300

    Ejercicio E, agregada tercera alternativa

--- exercise_1_Python.py
+++ exercise_1_Python.py
@@ -43,6 +43,16 @@ for i in range(10):
         suma+=numero
 print("La suma de los ultimos 5 numeros es: ", suma)
 
+""" E3) """
+nElementos = int(input("Ingrese porfavor la dimension de la lista: "))
+lista = list(range(0, nElementos))
+suma=0
+
+for i in range(nElementos):
+    lista = int(input(str(i+1)+". Ingrese porfavor 10 numeros enteros: "))
+    if (i>4):
+        suma+=lista
+print("La suma de los ultimos 5 numeros es: ",suma)
 
 
 """ F) """
