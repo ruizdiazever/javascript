0c9b170faca1f9f84b352d40c3ddadc19f7f638d
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-09-10 18:44:59 -0300

    Cambios menores

--- ejercicio_0/random.py
+++ ejercicio_0/random.py
@@ -1,17 +1,16 @@
 import random
-print("\nEjercicio E:")
-suma=0
-lista =[]
+print("\n Ejercicio E:")
+suma = 0
+lista = []
 
 for i in range(10):
-    num=random.randint(0,100)
-    print("numero generado: ", num)
-    lista.append(num)
-
+   num=random.randint(0,100)
+   print("numero generado: ",num)
+   lista.append(num)
 
 for i in range(10):
-    if (i>4):
-        suma+= lista[i]
-print("La suma de las ultimas 5 cifras es: ",suma)
+   if(i>4):
+       suma+= lista[i]
+print("La suma de las últimas 5 cifras es: ",suma)
 
-print("Lista completa \n ", lista)
+print("Lista completa \n",lista)
