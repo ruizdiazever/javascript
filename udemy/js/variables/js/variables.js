29e0416871af9b2e48847f1eb3ea6656b77219a9
ruizdiazever <ruizdiaz.oe@gmail.com>
2021-01-04 08:49:38 +0100

    Resume the js course

---
+++ udemy/js/variables/js/variables.js
@@ -0,0 +1,8 @@
+// VARIABLES EN JAVASCRIPT
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2019;
+var paisContinente = pais + " de " + continente
+
+console.log("Pais:", pais, "\nContinente:", continente, "\nAntiguedad:", antiguedad)
+console.log(paisContinente)
